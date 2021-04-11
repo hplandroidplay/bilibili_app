66946191db3c41fcda13e8ca1df9409b0660e6aa
hplandroidplay <shipotianhpl@163.com>
2021-04-11 10:16:10 +0800

    feat: app add video module (#4)

--- bilibili_app/lib/page/video_details_page.dart
+++ bilibili_app/lib/page/video_details_page.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
+
 import 'package:bilibili_app/model/video_model.dart';
+import 'package:bilibili_app/utils/view_util.dart';
+import 'package:bilibili_app/widget/appbar.dart';
+import 'package:bilibili_app/widget/custom_tab.dart';
+import 'package:bilibili_app/widget/navigation_bar.dart';
+import 'package:bilibili_app/widget/video/video_view.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class VideoDetailPage extends StatefulWidget {
@@ -10,11 +18,91 @@ class VideoDetailPage extends StatefulWidget {
   _VideoDetailPageState createState() => _VideoDetailPageState();
 }
 
-class _VideoDetailPageState extends State<VideoDetailPage> {
+class _VideoDetailPageState extends State<VideoDetailPage>
+    with TickerProviderStateMixin {
+  TabController _controller;
+  List tabs = ["简介", "评论288"];
+
+  @override
+  void initState() {
+    super.initState();
+    //黑色状态栏，仅Android
+    changeStatusBar(
+        color: Colors.black, statusStyle: StatusStyle.LIGHT_CONTENT);
+    _controller = TabController(length: tabs.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("VideoDetailPage:videoId=${widget.videoModel.vid}"),
+    return Scaffold(
+      body: MediaQuery.removePadding(
+          removeTop: Platform.isIOS,
+          context: context,
+          child: Column(
+            children: [
+              //iOS 黑色状态栏
+              NavigationBar(
+                color: Colors.black,
+                statusStyle: StatusStyle.LIGHT_CONTENT,
+                height: Platform.isAndroid ? 0 : 46,
+              ),
+              _buildVideoView(),
+              _buildTabNavigation(),
+            ],
+          )),
+    );
+  }
+
+  _buildVideoView() {
+    var model = widget.videoModel;
+    return VideoView(
+      model.url,
+      cover: model.cover,
+      overlayUI: videoAppBar(),
+    );
+  }
+
+  _buildTabNavigation() {
+    //使用Material实现阴影效果
+    return Material(
+      elevation: 5,
+      shadowColor: Colors.grey[100],
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.only(left: 20),
+        height: 39,
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _tabBar(),
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Icon(
+                Icons.live_tv_rounded,
+                color: Colors.grey,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _tabBar() {
+    return CustomTab(
+      tabs.map<Tab>((name) {
+        return Tab(
+          text: name,
+        );
+      }).toList(),
+      controller: _controller,
     );
   }
 }
